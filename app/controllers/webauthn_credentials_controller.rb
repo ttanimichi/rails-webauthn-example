1f82dd705716236524fd5754b2143c006fa57e87
Tsukuru Tanimichi <info@ttanimichi.com>
2026-06-27 21:37:02 +0900

    Require resident key and user verification for WebAuthn

--- app/controllers/webauthn_credentials_controller.rb
+++ app/controllers/webauthn_credentials_controller.rb
@@ -4,7 +4,11 @@ class WebauthnCredentialsController < ApplicationController
 
     options = WebAuthn::Credential.options_for_create(
       user: { id: current_user.webauthn_user_handle, name: current_user.name },
-      exclude: current_user.webauthn_credentials.pluck(:credential_id)
+      exclude: current_user.webauthn_credentials.pluck(:credential_id),
+      authenticator_selection: {
+        resident_key: "required",
+        user_verification: "required"
+      }
     )
 
     session[:creation_challenge] = options.challenge
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   # User Handle を Base64URL でエンコードして保存している
   # A user handle is an opaque byte sequence with a maximum size of 64 bytes, and is not meant to be displayed to the user.
-  # ref. https://www.w3.org/TR/webauthn-2/#user-handle
+  # ref. https://www.w3.org/TR/webauthn-3/#user-handle
   validates :webauthn_user_handle, format: { with: /\A[A-Za-z0-9_-]+\z/ }, allow_nil: true
 end
