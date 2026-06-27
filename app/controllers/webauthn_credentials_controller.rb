7f16e9abdad5857f9317aef4befba680678ceb65
Tsukuru Tanimichi <info@ttanimichi.com>
2026-06-27 22:53:27 +0900

    Document authenticator selection criteria in WebAuthn

--- app/controllers/webauthn_credentials_controller.rb
+++ app/controllers/webauthn_credentials_controller.rb
@@ -5,6 +5,8 @@ class WebauthnCredentialsController < ApplicationController
     options = WebAuthn::Credential.options_for_create(
       user: { id: current_user.webauthn_user_handle, name: current_user.name },
       exclude: current_user.webauthn_credentials.pluck(:credential_id),
+      # Authenticator Selection Criteria
+      # ref. https://www.w3.org/TR/webauthn-3/#dictdef-authenticatorselectioncriteria
       authenticator_selection: {
         resident_key: "required",
         user_verification: "required"
