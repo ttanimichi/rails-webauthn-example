a66232741459c4d7c4d4c0af8a3bef706b225f87
Tsukuru Tanimichi <info@ttanimichi.com>
2026-07-01 13:47:19 +0900

    Add WebAuthn metadata references

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,20 @@ class ApplicationController < ActionController::Base
     response = webauthn_credential.response
 
     session[:webauthn_metadata] = {
+      # ref. https://www.w3.org/TR/webauthn-3/#aaguid
+      # ref. https://passkeydeveloper.github.io/passkey-authenticator-aaguids/explorer/
       aaguid: response.respond_to?(:aaguid) ? response.aaguid : nil,
+
+      # ref. https://www.w3.org/TR/webauthn-3/#dom-publickeycredential-authenticatorattachment
       authenticator_attachment: webauthn_credential.authenticator_attachment,
+
+      # ref. https://www.w3.org/TR/webauthn-3/#dom-authenticatorattestationresponse-gettransports
       transports: response.respond_to?(:transports) ? response.transports : nil,
+
+      # ref. https://www.w3.org/TR/webauthn-3/#backup-eligibility
       backup_eligible: webauthn_credential.backup_eligible?,
+
+      # ref. https://www.w3.org/TR/webauthn-3/#backup-state
       backed_up: webauthn_credential.backed_up?
     }.compact
   end
